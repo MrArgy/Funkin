759db7079d170444e11ada9c7c575e0ffb17b32c
Tai <tmtudev@gmail.com>
2021-07-13 00:30:43 +0700

    add tricky shit

---
+++ source/fmf/songs/TrickyHell.hx
@@ -0,0 +1,93 @@
+package fmf.songs;
+
+import flixel.util.FlxColor;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import MenuCharacter.CharacterSetting;
+import fmf.characters.*;
+
+class TrickyHell extends SongPlayer
+{
+
+    override function getDadTex()
+	{
+		var frames = Paths.getSparrowAtlas('clown/tiky', 'mods');
+		dad.frames = frames;
+	}
+
+	override function loadMap()
+	{
+		playState.defaultCamZoom = 0.5;
+		var bg:FlxSprite = new FlxSprite(-400, -200).loadGraphic(Paths.image('clown/red', 'mods'));
+		bg.antialiasing = true;
+
+		bg.scale.y = 1;
+		bg.scale.x = 1;
+		playState.add(bg);
+		
+		var stageFront:FlxSprite = new FlxSprite(0, 0).loadGraphic(Paths.image('clown/hellclown/island_but_red', 'mods'));
+
+		stageFront.x -= 500;
+		stageFront.y = 200; 
+
+		stageFront.scale.x = 3;
+		stageFront.scale.y = 3;
+		playState.add(stageFront);
+		
+
+	}
+
+	override function createDadAnimations():Void
+	{
+		var animation = dad.animation;
+		animation.addByPrefix('idle', 'Idle', 18, false);
+		animation.addByPrefix('singUP', 'Proper Up', 24, false);
+		animation.addByPrefix('singRIGHT', 'Proper Right', 24, false);
+		animation.addByPrefix('singLEFT', 'Proper Left', 24, false);
+		animation.addByPrefix('singDOWN', 'Proper Down', 24, false);
+		dad.animation = animation;
+	}
+
+	override function createDadAnimationOffsets():Void
+	{
+		dad.addOffset('idle', 0, 236);
+		dad.addOffset('singUP', 108, 13);
+		dad.addOffset('singRIGHT', 12, 156);
+		dad.addOffset('singLEFT', -50, 309);
+		dad.addOffset('singDOWN', 10, 50);
+
+		dad.dance();
+
+		dad.scale.x = 4;
+		dad.scale.y = 4;
+
+		dad.x -= 500;
+		dad.y += 400;
+	}
+
+	public override function createCharacters()
+	{
+		super.createCharacters();
+		bf.x += 100;
+		bf.y += 50;
+	}
+
+	public override function getDadIcon(icon:HealthIcon)
+	{
+		icon.loadGraphic(Paths.image('iconGrid'), true, 150, 150);
+		icon.animation.add('dad', [17, 18], 0, false, false);
+		icon.animation.play("dad");
+	}
+
+	public override function setDadMenuCharacter(dad:MenuCharacter)
+	{
+		super.setDadMenuCharacter(dad);
+
+		var frames = Paths.getSparrowAtlas('whitty/whitty', 'mods');
+		dad.frames = frames;
+
+		dad.animation.addByPrefix('dad', "Whitty idle dance BLACK LINE", 24);
+		dad.animation.play('dad');
+		setMenuCharacter(dad, new CharacterSetting(-200, 25, 1));
+	}
+}
---
+++ source/fmf/songs/TrickyMask.hx
@@ -0,0 +1,95 @@
+package fmf.songs;
+
+import flixel.util.FlxColor;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import MenuCharacter.CharacterSetting;
+import fmf.characters.*;
+
+class TrickyMask extends SongPlayer
+{
+
+    override function getDadTex()
+	{
+		var tex = Paths.getSparrowAtlas('clown/TrickyMask', 'mods');
+		dad.frames = tex;
+	}
+
+
+	override function loadMap()
+	{
+		var bg:FlxSprite = new FlxSprite(-400, -200).loadGraphic(Paths.image('clown/red', 'mods'));
+		bg.antialiasing = true;
+		bg.scale.y = 1;
+		bg.scale.x = 1;
+		playState.add(bg);
+
+		var fg:FlxSprite = new FlxSprite();
+		fg.frames =  Paths.getSparrowAtlas('clown/tricky_floor', 'mods');
+		fg.animation.addByPrefix('idle', 'Symbol 1');
+		fg.antialiasing = true;
+
+		fg.y = 600;
+		fg.x = 150;
+		fg.scale.y = 1.75;
+		fg.scale.x = 1.75;
+		playState.add(fg);
+
+	}
+
+	override function createDadAnimations():Void
+	{
+		var animation = dad.animation;
+		animation.addByPrefix('idle', 'Idle', 18, false);
+		animation.addByPrefix('singUP', 'Sing Up', 24, false);
+		animation.addByPrefix('singRIGHT', 'Sing Right', 24, false);
+		animation.addByPrefix('singLEFT', 'Sing Left', 24, false);
+		animation.addByPrefix('singDOWN', 'Sing Down', 24, false);
+		dad.animation = animation;
+	}
+
+	override function createDadAnimationOffsets():Void
+	{
+			
+	
+		dad.addOffset('idle', -3, 4);
+		dad.addOffset('singUP', 35, 24);
+		dad.addOffset('singRIGHT', 4, -24);
+		dad.addOffset('singLEFT', 102, 16);
+		dad.addOffset('singDOWN', 10, -15);
+		
+		dad.x += 3;
+		dad.y -= 4;
+		dad.scale.x = 1.5;
+		dad.scale.y = 1.5;
+
+		dad.dance();
+
+	}
+
+	public override function createCharacters()
+	{
+		super.createCharacters();
+		bf.x += 100;
+		bf.y += 50;
+	}
+
+	public override function getDadIcon(icon:HealthIcon)
+	{
+		icon.loadGraphic(Paths.image('iconGrid'), true, 150, 150);
+		icon.animation.add('dad', [17, 18], 0, false, false);
+		icon.animation.play("dad");
+	}
+
+	public override function setDadMenuCharacter(dad:MenuCharacter)
+	{
+		super.setDadMenuCharacter(dad);
+
+		var frames = Paths.getSparrowAtlas('whitty/whitty', 'mods');
+		dad.frames = frames;
+
+		dad.animation.addByPrefix('dad', "Whitty idle dance BLACK LINE", 24);
+		dad.animation.play('dad');
+		setMenuCharacter(dad, new CharacterSetting(-200, 25, 1));
+	}
+}
